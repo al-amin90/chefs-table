c6b35b6bd91195e1611bc823ebbdac8d5bd36795
al-amin90 <ijesun30@gmail.com>
2024-03-15 20:23:47 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <a className="btn btn-ghost text-xl">Recipe Calories</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li><a>Recipes</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
