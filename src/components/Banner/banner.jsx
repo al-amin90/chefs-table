d6befc370dbeb45dbd5de7c1b32d25093c897e9d
al-amin90 <ijesun30@gmail.com>
2024-03-16 05:47:28 +0600

    banner added

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -6,15 +6,15 @@ const Banner = () => {
     return (
         <div className='relative'>
             <div className='z-10 text-white text-center inset-center w-9/12'>
-                <h1 className='text-5xl font-bold mb-5 leading-tight'>Discover an exceptional cooking class tailored for you!</h1>
-                <p className='font-light'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
-                <div className='mt-7'>
-                    <button className='rounded-full btn bg-[#0BE58A] border-none px-6 font-semibold text-base mr-3'>Explore Now</button>
-                    <button className='rounded-full btn text-white border-white btn-outline px-6 font-semibold text-base ml-4'>Explore Now</button>
+                <h1 className='text-base lg:text-5xl font-bold mb-5 leading-tight'>Discover an exceptional cooking class tailored for you!</h1>
+                <p className='font-light text-xs  md:text-base'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+                <div className='lg:mt-7'>
+                    <button className='rounded-full btn bg-[#0BE58A] border-none px-6 font-semibold text-base mt-2 md:mr-3'>Explore Now</button>
+                    <button className='rounded-full btn text-white border-white btn-outline px-6 font-semibold text-base mt-2 md:ml-4'>Our Feedback</button>
                 </div>
             </div>
 
-            <img className='w-full' src={image} alt="" />
+            <img className='w-full md:h-auto h-[250px]' src={image} alt="" />
 
             
         </div>
